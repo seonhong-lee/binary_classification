e7af208a8c0fa4b1ff1682918f8383fe31eb7f94
seonhong lee <dltjsghd12@g.skku.edu>
2022-04-14 22:48:46 +0900

    2022_04_14_data_loader

--- data_loader.py
+++ data_loader.py
@@ -27,3 +27,24 @@ class DataLoader(object):
             include_lengths=False,
             eos_token="<EOS>" if use_eos else None
         )
+        
+        train, valid = data.TabularDataset(
+            path=train_file_name,
+            format='tsv',
+            fileds=[
+                ('label', self.label),
+                ('text', self.text),
+            ],
+        ).split(split_ratio=(1-valid_ratio))
+        
+        self.train_loader, self,valid_loader = data.BucketIteratior.splits(
+            (train, valid),
+            batch_size=batch_size,
+            sevice='cuda:%d' % device if device >= 0 else 'cpu',
+            shuffle=shuffle,
+            sort_key=lambda x: len(x.text),
+            sort_within_batch=True,
+        )
+        
+        self.label.build_vocab(train)
+        self.text_build_vocab(train, max_size=max_vocab, min_freq=min_freq)
