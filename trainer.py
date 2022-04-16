7757f7006b634a7d2cc7e76c625b1b5140f4dfe7
seonhong lee <dltjsghd12@g.skku.edu>
2022-04-16 23:24:49 +0900

    2022_04_16_trainer.py

--- trainer.py
+++ trainer.py
@@ -63,4 +63,33 @@ class InitEngine(Engine):
     
     @staticmethod
     def validate(engien, mini_batch):
-        pass
+        engine.model.eval()
+        
+        with torch.no_grad():
+            x, y = mini_batch.text, mini_batch.label
+            x, y = x.to(engine.device), y.to(engine.device)
+            
+            x = x[:, :engine.config.max_length]
+            
+            y_hat = engine.model(x)
+            
+            loss = engine.crit(y_hat, y) 
+            
+            if isinstance(y, torch.LongTensor) or isinstance(y, torch.cuda.LongTensor):
+                accuracy = (torch.argmax(y_hat, dim=-1) == y).sum() / float(y.size(0))
+            else:
+                accuracy = 0
+                
+            return {
+                'loss': float(loss),
+                'accuracy': float(accuracy)
+            }
+            
+    @staticmethod
+    def attach(train_engine, validation_engine, verbose=VERBOSE_BATCH_WISE):
+        
+        def attach_running_average(engine, metric_name):
+            RunningAverage(output_transform=lambda x: x[metric_name]).attach(
+                engine,
+                metric_name
+            )
